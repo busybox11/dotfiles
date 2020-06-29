58c353b6a7af7eb72c0807be8db1cdb2499f1863
busybox <planetgeek11@gmail.com>
2020-06-29 17:48:55 +0200

    Fixed not working script

--- bin/unsplash.sh
+++ bin/unsplash.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 if ping -q -c 1 -W 1 google.com >/dev/null; then
-	wget -O /tmp/wallpaper.jpg https://source.unsplash.com/3840x2160/?wallpaper
-	feh --bg-fill /tmp/wallpaper.jpg
-	wal -i /tmp/wallpaper.jpg -q
+    wget -O /tmp/wallpaper.jpg http://source.unsplash.com/3840x2160/?wallpaper --no-check-certificate
+    feh --bg-fill /tmp/wallpaper.jpg
+    wal -i /tmp/wallpaper.jpg -q -c --backend colorz
 else
-	feh --bg-fill YOUR_FALLBACK_WALLPAPER_PATH
-    wal -i YOUR_FALLBACK_WALLPAPER_PATH -q
+    feh --bg-fill ~/Pictures/Wallpapers/img_wallpapers_basic\ wallpaper_library.png
+    wal -i ~/Pictures/Wallpapers/img_wallpapers_basic\ wallpaper_library.png -q -c --backend colorz
 fi
-
-
