84e20825787a492042d0363de76fbb7666da4aee
busybox <planetgeek11@gmail.com>
2020-06-29 18:18:24 +0200

    Hotfix

--- bin/unsplash.sh
+++ bin/unsplash.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+rm -rf .cache/wal
 if ping -q -c 1 -W 1 google.com >/dev/null; then
     wget -O /tmp/wallpaper.jpg http://source.unsplash.com/3840x2160/?wallpaper --no-check-certificate
+    wal -i "/tmp/wallpaper.jpg" -n
     feh --bg-fill /tmp/wallpaper.jpg
-    wal -i /tmp/wallpaper.jpg -q -c --backend colorz
 else
+    wal -i "~/Pictures/Wallpapers/img_wallpapers_basic wallpaper_library.png" -n
     feh --bg-fill ~/Pictures/Wallpapers/img_wallpapers_basic\ wallpaper_library.png
-    wal -i ~/Pictures/Wallpapers/img_wallpapers_basic\ wallpaper_library.png -q -c --backend colorz
 fi
