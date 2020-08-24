b707fb50506b7213ee85f9ef64d4761974af7b73
busybox <planetgeek11@gmail.com>
2020-08-24 23:58:40 +0200

    Run pywal after wallpaper change

--- bin/unsplash.sh
+++ bin/unsplash.sh
@@ -3,9 +3,9 @@
 rm -rf .cache/wal
 if ping -q -c 1 -W 1 google.com >/dev/null; then
     wget -O /tmp/wallpaper.jpg http://source.unsplash.com/3840x2160/?wallpaper --no-check-certificate
-    wal -i "/tmp/wallpaper.jpg" -n
     feh --bg-fill /tmp/wallpaper.jpg
+    wal -i "/tmp/wallpaper.jpg" -n
 else
-    wal -i "~/Pictures/Wallpapers/img_wallpapers_basic wallpaper_library.png" -n
     feh --bg-fill ~/Pictures/Wallpapers/img_wallpapers_basic\ wallpaper_library.png
+    wal -i "~/Pictures/Wallpapers/img_wallpapers_basic wallpaper_library.png" -n
 fi
