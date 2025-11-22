9b384de28321d045f05defeaf594872f50e41c78
busybox11 <busybox11th@gmail.com>
2025-11-22 03:36:34 +0100

    style: new desktop background . i love my computer

--- scripts/modes/night.sh
+++ scripts/modes/night.sh
@@ -16,12 +16,12 @@ bash $night_script set_brightness 1Q1Q7HA012666 0
 bash $night_script set_brightness 3CQ6030YX6 0
 
 # hyprpaper
-night_wallpaper="~/Pictures/wallpapers/james_k_friend_night.png"
+night_wallpaper="~/Pictures/wallpapers/ilmc_desktop_bg.png"
 hyprctl hyprpaper preload $night_wallpaper
 hyprctl hyprpaper wallpaper ,$night_wallpaper
 
 # get remaining loaded wallpapers, unload all but the night wallpaper
 remaining_wallpapers=$(hyprctl hyprpaper listloaded | grep -v $night_wallpaper)
 for wallpaper in $remaining_wallpapers; do
-    hyprctl hyprpaper unload $wallpaper
+  hyprctl hyprpaper unload $wallpaper
 done
