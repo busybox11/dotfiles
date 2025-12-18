4d0c062cbf789737f4992f8062fa60dd9a321e45
busybox11 <busybox11th@gmail.com>
2025-12-19 00:58:55 +0100

    feat(pacman): kernel-modules-hook and clean service enabling script for dkms

--- scripts/setup/packages.sh
+++ scripts/setup/packages.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 usage() {
-    echo "usage: $0 <target_list>"
-    exit 1
+  echo "usage: $0 <target_list>"
+  exit 1
 }
 
 target_list=$1
 if [ -z "$target_list" ]; then
-    usage
+  usage
 fi
 
 if [ "$target_list" == "all" ]; then
-    target_lists=(wm programs shell apps)
+  target_lists=(wm programs shell apps utils)
 else
-    target_lists=($target_list)
+  target_lists=($target_list)
 fi
 
 # install packages
 for list in ${target_lists[@]}; do
-    echo "installing packages from $list"
-    packages=$(cat meta/packages/$list)
-    yay -S --needed $packages
+  echo "installing packages from $list"
+  packages=$(cat meta/packages/$list)
+  yay -S --needed $packages
 done
+
