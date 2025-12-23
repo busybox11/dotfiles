14b848e6e6a8a4f680277b573946e09a46d61ff1
busybox11 <busybox11th@gmail.com>
2025-12-23 17:57:58 +0100

    fix(nvim): dont autochdir

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.autochdir = true
+-- vim.o.autochdir = true
