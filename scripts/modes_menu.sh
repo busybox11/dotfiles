45c09d154f38b4a0a70015b9f0fbceb50a213eb0
busybox11 <busybox11th@gmail.com>
2025-12-23 15:56:33 +0100

    feat(scripts/modes): only show topmost files

--- scripts/modes_menu.sh
+++ scripts/modes_menu.sh
@@ -2,12 +2,13 @@
 
 # opens mode dmenu selector with vicinae
 # all modes are scripts stored in the ./modes directory relative to this script
+# only shows scripts in the root of the modes directory, ignores subdirectories (hooks, utils...)
 
-modes_dir=$(dirname "$0")/modes
-menu_items=$(ls "$modes_dir" | sed 's/\.sh$//')
+modes_dir="$(dirname "$0")/modes"
+menu_items=$(find "$modes_dir" -maxdepth 1 -type f -name "*.sh")
 
 selected=$(echo -e "$menu_items" | vicinae dmenu --placeholder "toggle mode")
 
 if [ -n "$selected" ]; then
-    bash "$modes_dir/$selected.sh"
+  bash "$selected"
 fi
