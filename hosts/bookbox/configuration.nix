27be322f71be6bff3492af744a02773d146a15d1
busybox11 <busybox11th@gmail.com>
2026-05-03 21:56:16 +0200

    feat(nix/bookbox): intel laptop power management

--- hosts/bookbox/configuration.nix
+++ hosts/bookbox/configuration.nix
@@ -18,5 +18,8 @@ in
 
   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEXbYlSOvJuaxsDejRybBkLQwbA18fhTE3j1oIb1cR4K" ];
 
+  # bookbox is an intel laptop (ew)
+  services.thermald.enable = true;
+
   system.stateVersion = "25.05";
 }
