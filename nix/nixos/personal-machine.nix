6cd68d62e992d2bc107f0f7984aba8bf81baa81b
busybox11 <busybox11th@gmail.com>
2026-05-03 04:02:23 +0200

    feat(nix/personal-machine): basic nixos target profile def

---
+++ nix/nixos/personal-machine.nix
@@ -0,0 +1,24 @@
+# HM + primary user. flakeHost follows networking.hostName (= hostName).
+{
+  hostName,
+  username,
+  dotfilesPath ? "/home/${username}/dev/dotfiles",
+  homeDirectory ? "/home/${username}",
+}:
+{ self, hosts, ... }:
+{
+  networking.hostName = hostName;
+
+  users.users.${username} = {
+    isNormalUser = true;
+    home = homeDirectory;
+    extraGroups = [ "sudo" "wheel" "networkmanager" ];
+  };
+
+  home-manager.extraSpecialArgs = {
+    inherit hosts self username homeDirectory dotfilesPath;
+    flakeHost = hostName;
+  };
+
+  home-manager.users.${username}.imports = [ ../home.nix ];
+}
