55cd57ff91c6b74ccf7c6f5caa427787f34086bf
busybox11 <29630035+busybox11@users.noreply.github.com>
2026-07-04 22:04:58 +0200

    feat(nix/hm/zen): expand browser policies, search, fonts, and mods

--- nix/hm/modules/zen.nix
+++ nix/hm/modules/zen.nix
@@ -6,10 +6,32 @@
 
   programs.zen-browser = {
     enable = true;
+    setAsDefaultBrowser = true;
+
     policies = {
-      DefaultSearchEngine = "Kagi";
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
     };
+    
     profiles.default = {
+      search = {
+        force = true;
+        default = "Kagi";
+        engines = {
+          "Kagi" = {
+            urls = [
+              { 
+                template = "https://kagi.com/search?q={searchTerms}"; 
+              }
+            ];
+            icon = "https://assets.kagi.com/v2/favicon-32x32.png";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = [ "@kg" ];
+          };
+        };
+      };
+
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         bitwarden
@@ -21,6 +43,25 @@
         stylus
         material-icons-for-github
         tampermonkey
+      ];
+
+      settings = {
+        "browser.aboutConfig.showWarning" = false;
+        "widget.dmabuf.force-enabled" = true;
+        "font.name.monospace.x-western" = "Cascadia Code NF";
+        "font.name.sans-serif.x-western" = "SF Pro Text";
+        "font.name.serif.x-western" = "SF Pro Text";
+        "zen.theme.content-element-separation" = 0;
+        "zen.theme.gradient.show-custom-colors" = true;
+        "zen.theme.use-system-colors" = true;
+        "zen.urlbar.show-domain-only-in-sidebar" = false;
+      };
+
+      mods = [
+        "81fcd6b3-f014-4796-988f-6c3cb3874db8"
+        "f7c71d9a-bce2-420f-ae44-a64bd92975ab"
+        "dbe05f83-b471-4278-a3f9-e5ed244b0d6c"
+        "03a8e7ef-cf00-4f41-bf24-a90deeafc9db"
       ];
     };
   };
