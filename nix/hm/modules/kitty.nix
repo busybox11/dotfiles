cf7184a01b86592f1fa8ca50bb697f29b7b70aa7
busybox11 <29630035+busybox11@users.noreply.github.com>
2026-08-08 12:05:15 +0200

    feat(nix/kitty): use matugen colors

--- nix/hm/modules/kitty.nix
+++ nix/hm/modules/kitty.nix
@@ -5,6 +5,8 @@
     package = pkgs.kitty;
 
     settings = {
+      include = "colors.conf";
+
       background_opacity = 0.75;
       font_size = 10.5;
       repaint_delay = 5;
@@ -14,31 +16,6 @@
       scrollbar_gap = 0.3;
       scrollbar_track_hover_opacity = 0.05;
       notify_on_cmd_finish = "unfocused";
-
-      cursor = "#e8e2d5";
-      cursor_text_color = "#ccc6b5";
-      foreground = "#e8e2d5";
-      background = "#090801";
-      selection_foreground = "#353117";
-      selection_background = "#cfc7a2";
-      url_color = "#d6c870";
-
-      color0 = "#35301f";
-      color1 = "#ff8678";
-      color2 = "#beb380";
-      color3 = "#bbb488";
-      color4 = "#cbb948";
-      color5 = "#86be9a";
-      color6 = "#84bf9f";
-      color7 = "#d6cbb4";
-      color8 = "#ccc6b5";
-      color9 = "#ffe2de";
-      color10 = "#e0dbc4";
-      color11 = "#8d8450";
-      color12 = "#e1d798";
-      color13 = "#c8e2d2";
-      color14 = "#4c916c";
-      color15 = "#faf9f6";
     };
 
     extraConfig = ''
