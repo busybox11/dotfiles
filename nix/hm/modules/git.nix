191c1ee577ceb0f729c83dbc911f59a120da6923
busybox11 <29630035+busybox11@users.noreply.github.com>
2026-08-10 01:32:28 +0200

    feat(nix/hm/modules/git): add signing configuration for Git

--- nix/hm/modules/git.nix
+++ nix/hm/modules/git.nix
@@ -1,4 +1,9 @@
-{ pkgs, dotfilesPath, lib, ... }:
+{
+  pkgs,
+  dotfilesPath,
+  lib,
+  ...
+}:
 {
   programs.ssh = {
     enable = true;
@@ -17,6 +22,9 @@
     settings = {
       user.name = "busybox11";
       user.email = "29630035+busybox11@users.noreply.github.com";
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+      commit.gpgsign = true;
     };
 
     includes = [
