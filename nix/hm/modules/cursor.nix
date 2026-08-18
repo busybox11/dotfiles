c0d8ab25e90fac30254b35006443221906f046dd
busybox11 <29630035+busybox11@users.noreply.github.com>
2026-08-18 10:37:55 +0200

    fix(nix/cursor): re-export shell env for sandbox

--- nix/hm/modules/cursor.nix
+++ nix/hm/modules/cursor.nix
@@ -1,11 +1,28 @@
-{ pkgs, flakeHost, ... }:
+{
+  pkgs,
+  lib,
+  flakeHost,
+  ...
+}:
 let
   vscodeConfig = import ./vscode-config.nix { inherit pkgs flakeHost; };
+  cursor = pkgs.code-cursor;
 in
 {
   programs.cursor = {
     enable = true;
-    package = pkgs.code-cursor;
+    # https://forum.cursor.com/t/cursors-sandbox-unable-to-run-any-command-missing-zsh-mount-in-sandbox/159727/5
+    package = pkgs.symlinkJoin {
+      inherit (cursor) name pname version;
+      paths = [ cursor ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        rm -f $out/bin/${cursor.meta.mainProgram}
+        makeWrapper ${lib.getExe cursor} $out/bin/${cursor.meta.mainProgram} \
+          --set SHELL ${lib.getExe pkgs.zsh}
+      '';
+      meta = cursor.meta;
+    };
 
     profiles.default = {
       userSettings = vscodeConfig.sharedSettings;
